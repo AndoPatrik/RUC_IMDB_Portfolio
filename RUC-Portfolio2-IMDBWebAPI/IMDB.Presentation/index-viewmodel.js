3dd86569e32339ea048319c1f3b49422af6d6a93
AndoPatrik <screadern@gmail.com>
2022-01-22 17:01:50 +0100

    Document formating + movies displayed in rows

--- RUC-Portfolio2-IMDBWebAPI/IMDB.Presentation/index-viewmodel.js
+++ RUC-Portfolio2-IMDBWebAPI/IMDB.Presentation/index-viewmodel.js
@@ -1,29 +1,27 @@
 import { fetchData } from "./httpRequest.js";
 
-var IndexViewModel = function() {
+var IndexViewModel = function () {
     var self = this;
     self.movies = ko.observableArray();
     self.nextLink = ko.observable();
     self.previousLink = ko.observable();
 
-    self.fetchMovies = async function(path){
+    self.fetchMovies = async function (path) {
         var result = await fetchData(path)
         self.movies(result.data)
         self.nextLink(result.nextPage)
         self.previousLink(result.previousPage)
-        console.log(this.nextLink())
-        console.log(self.previousLink())
     }
 
-    self.handleNext = function(){
+    self.handleNext = function () {
         this.fetchMovies(self.nextLink());
     }
 
-    self.handlePrevious = function(){
+    self.handlePrevious = function () {
         this.fetchMovies(self.previousLink())
     }
 
-    window.onload = this.fetchMovies("https://localhost:7167/api/Titles?PageNumber=1&PageSize=10");
+    window.onload = this.fetchMovies("https://localhost:7167/api/Titles?PageNumber=1&PageSize=5");
 };
- 
+
 ko.applyBindings(new IndexViewModel());
