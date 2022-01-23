93f0576f5be328a9ab1fda079c2feb620860bfb0
AndoPatrik <screadern@gmail.com>
2022-01-23 17:26:26 +0100

    Change default json serializer to newtonsoft

--- RUC-Portfolio2-IMDBWebAPI/IMDBWebAPI/Program.cs
+++ RUC-Portfolio2-IMDBWebAPI/IMDBWebAPI/Program.cs
@@ -30,7 +30,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -125,6 +125,8 @@ builder.Services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationMa
 
 //CORS
 builder.Services.AddCors();
+
+
 
 var app = builder.Build();
 
